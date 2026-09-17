6561407e3e83e9e01246b0f7abd1f5e8d8cf8507
Yvette Jing <mutongjing@gmail.com>
2026-09-17 13:12:18 -0700

    Add files via upload

---
+++ ConvertToDegree.java
@@ -0,0 +1,19 @@
+	import java.util.Scanner;
+	
+	public class ConvertToDegree {
+		public static void main(String[] args) {
+			
+			Scanner input = new Scanner(System.in);
+			
+			//prompt the user and get the value
+			System.out.print("temperature in degree Celsius: ");
+			double Celsius = input.nextDouble();
+			
+			//convert and outputthe result
+			double Fahrenheit = (Celsius * 9 / 5) + 32;
+			System.out.println(Celsius + " C" + " = " + Fahrenheit + " F");
+
+			input.close();
+		}
+	}
+
