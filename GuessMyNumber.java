8d1b965fabda5c42f818c1143315aee08729e117
Yvette Jing <mutongjing@gmail.com>
2026-09-17 13:45:13 -0700

    Add files via upload

---
+++ GuessMyNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
+
+	public class GuessMyNumber {
+		
+		public static void main(String[] args) {
+			Scanner input = new Scanner(System.in);
+			
+			// pick a random number
+			Random random = new Random();
+			int number = random.nextInt(100) + 1;
+			
+			// prompt the user to get their guess
+			System.out.println("I'm thinking of a num5ber between 1 and 100 (including both). Can you guess what it is?");
+			System.out.print("Type a number: ");
+			int guess = input.nextInt();
+			
+			//output
+			System.out.println("The number I was thinking of is: " + number);
+			System.out.println("You were off by: " + Math.abs(number - guess));
+			
+		}
+	}
